84a9c4c558cb9cdd1f8ee3138b6f7161da60f170
Divum <rkeerthick1@gmail.com>
2023-11-22 16:33:18 +0530

    json port changed

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const fetchPosts = () => {
-  const response = axios.get("http://localhost:4001/posts");
+  const response = axios.get("http://localhost:3939/posts");
   return response;
 };
 
 export const addPost = (data: {}) => {
-  const response = axios.post("http://localhost:4001/posts", data);
+  const response = axios.post("http://localhost:3939/posts", data);
   return response;
 };
 
 export const deletePost = (id: string) => {
-  const response = axios.delete(`http://localhost:4001/posts/${id}`);
+  const response = axios.delete(`http://localhost:3939/posts/${id}`);
   return response;
 };
 
 export const fetchPostByID = (id: number) => {
-  const response = axios.get(`http://localhost:4001/posts/${id}`);
+  const response = axios.get(`http://localhost:3939/posts/${id}`);
   return response;
 };
