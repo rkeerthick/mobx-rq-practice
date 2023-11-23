248a644787ebc5f70ea31304b322b886b4b0ebc9
Divum <rkeerthick1@gmail.com>
2023-11-23 12:01:25 +0530

    edit is passed

--- src/Pages/AddPostPage/AddPost.page.tsx
+++ src/Pages/AddPostPage/AddPost.page.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./AddPost.scss";
 import useStore from "../../Hooks/UseStore";
 // import { addPost } from "../../utils/functions";
@@ -12,33 +12,41 @@ import { fetchPostByID } from "../../utils/functions";
 
 const AddPost = observer(() => {
   const { id } = useParams();
+
   const ID: number = id ? +id : 0;
-  console.log(ID, "id");
+  
   const {
     rootStore: { postsStore },
   } = useStore();
 
-  const { data, isFetched } = useQuery(["post-detail ", id], () => fetchPostByID(ID));
+  const { data, isFetched, isLoading, isFetching } = useQuery(
+    ["post-detail ", id],
+    () => fetchPostByID(ID)
+  );
 
   console.log(data, "data");
 
-  let dervTitle = "";
-  let dervContent = "";
-  dervTitle = data?.data?.title;
-  dervContent = data?.data?.content;
-  
-  console.log(dervContent, ' ,', dervTitle)
-
   const { mutate } = useAddPost();
-  // const [userId, setUserId] = useState('');
-  const [title, setTitle] = useState(dervTitle);
-  const [content, setContent] = useState(dervContent);
+
+  useEffect(() => {
+    
+    let dervTitle = "";
+    let dervContent = "";
+    console.log(data?.data?.title, "title");
+    if (ID > 0) {
+      dervTitle = data?.data?.title;
+      dervContent = data?.data?.content;
+      setTitle(dervTitle);
+      setContent(dervContent);
+    }
+    console.log(dervContent, " ,", dervTitle, "dataas");
+  }, [data]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   const navigate = useNavigate();
 
-  // const handleUserId = (e: any) => {
-  //   setUserId(e.target.value);
-  // };
   const handleTitle = (e: any) => {
     setTitle(e.target.value);
   };
@@ -59,52 +67,37 @@ const AddPost = observer(() => {
     mutate(data);
     navigate("/");
 
-    // const response = await addPost(data);
-    // console.log(response.data, 'data')
-    // posts.push(response.data);
-    // console.log(posts, 'post')
   };
 
   return (
     <div className="add-post">
       <div className="add-post__container">
-        {isFetched && (
-          <>
-            <span className="add-post__container__title">Add Post</span>
-            <form>
-              {/* <div className="add-post__container__input">
-            <label htmlFor="">User ID</label>
-            <input
-              type="text"
-              placeholder="User Id"
-              value={userId}
-              onChange={handleUserId}
-            />
-          </div> */}
-              <div className="add-post__container__input">
-                <label htmlFor="">Title</label>
-                <input
-                  type="text"
-                  placeholder="Title"
-                  value={title}
-                  onChange={handleTitle}
-                />
-              </div>
-              <div className="add-post__container__input">
-                <label htmlFor="">Description</label>
-                <textarea
-                  placeholder="Content"
-                  rows={6}
-                  value={content}
-                  onChange={handleContent}
-                />
-              </div>
-              <button type="button" onClick={handleSubmit}>
-                Add
-              </button>
-            </form>
-          </>
-        )}
+        <>
+          <span className="add-post__container__title">Add Post</span>
+          <form>
+            <div className="add-post__container__input">
+              <label htmlFor="">Title</label>
+              <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={handleTitle}
+              />
+            </div>
+            <div className="add-post__container__input">
+              <label htmlFor="">Description</label>
+              <textarea
+                placeholder="Content"
+                rows={6}
+                value={content}
+                onChange={handleContent}
+              />
+            </div>
+            <button type="button" onClick={handleSubmit}>
+              Add
+            </button>
+          </form>
+        </>
       </div>
     </div>
   );
