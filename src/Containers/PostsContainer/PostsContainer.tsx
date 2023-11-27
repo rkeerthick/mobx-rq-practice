c515c2a64562b61e9615e7db368318fcdcd34346
Divum <rkeerthick1@gmail.com>
2023-11-27 17:57:01 +0530

    search not found added

--- src/Containers/PostsContainer/PostsContainer.tsx
+++ src/Containers/PostsContainer/PostsContainer.tsx
@@ -3,6 +3,9 @@ import Post from "../../Components/Post/Post";
 import useStore from "../../Hooks/UseStore";
 import { map } from "lodash";
 import { IPost } from "../../Types";
+import { useEffect } from "react";
+import NoDataFound from "../../assets/gif/no-result-found.gif";
+import Loader from "../../assets/gif/loading.gif";
 
 const PostsContainer = ({
   data,
@@ -15,17 +18,23 @@ const PostsContainer = ({
     rootStore: { postsStore, loginStore },
   } = useStore();
   postsStore.setPosts(data);
-  let result = data;
+  let result;
   if (loginStore?.getUserID > 0) {
-    result = data?.filter((d: any) => d.userId !== loginStore?.getUserID);
+    result = data?.filter((d: any) => d.userId === loginStore?.getUserID);
   }
   console.log(result, "resulst");
   if (isLoading || isFetching) {
     return <h1>Loading</h1>;
   }
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     <img src={Loader} alt="" />;
+  //   }, 3000);
+  // }, []);
   return (
     <div className="posts-container">
-      {
+      {result.length === 0 && <img src={NoDataFound} alt="" />}
+      {result &&
         map(result, (post) => (
           <Post
             key={post.id}
@@ -33,8 +42,7 @@ const PostsContainer = ({
             title={post.title}
             content={post.content}
           />
-        ))
-      }
+        ))}
     </div>
   );
 };
