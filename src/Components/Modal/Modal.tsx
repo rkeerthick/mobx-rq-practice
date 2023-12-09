04daa3b2710d0e3ca9c45eb134002d352a668f38
rkeerthick <rkeerthick1@gmail.com>
2023-12-10 02:36:43 +0530

    latest commit

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -14,6 +14,17 @@ const MODAL_STYLES_DANGER = css({
   zIndex: 1000,
 });
 
+const MODAL_STYLES_WARNING = css({
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#d08f4d",
+  padding: "20px",
+  borderRadius: "8px",
+  zIndex: 1000,
+});
+
 const OVERLAY_STYLES = css({
   position: "fixed",
   top: 0,
@@ -29,7 +40,13 @@ const Modal = ({ children, isOpen, type }: modalProps) => {
     ? createPortal(
         <>
           <div className={OVERLAY_STYLES}></div>
-          <div className={MODAL_STYLES_DANGER}>{children}</div>
+          <div
+            className={
+              type === "danger" ? MODAL_STYLES_DANGER : MODAL_STYLES_WARNING
+            }
+          >
+            {children}
+          </div>
         </>,
         document.getElementById("modal")!
       )
